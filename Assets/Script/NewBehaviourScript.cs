c1744a84ca3a55637f6965f8c58309d932a6e18e
Yeonju Lee <63062901+hetetet@users.noreply.github.com>
2023-02-11 17:45:26 +0900

    edit ground
    땅 위치 좀 수정함

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Jinwoo's nose is extremly big");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
